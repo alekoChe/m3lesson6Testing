1f5790ec8ba2b5ada3d60ffaa3328583b4fec87f
alekoChe <aleko2117bis@gmail.com>
2021-12-14 10:11:31 +0300

    убрал лишнее

--- src/main/java/tecting/ArrayOfOnesAndFours.java
+++ src/main/java/tecting/ArrayOfOnesAndFours.java
@@ -6,12 +6,18 @@ import java.util.List;
 
 public class ArrayOfOnesAndFours {
 
-    private int[] inputArray;
-
     public static void main(String[] args) {
+        final int[] inputArray1 = new int[]{1, 1, 1};
+        final int[] inputArray2 = new int[]{4};
+        final int[] inputArray3 = new int[]{1, 4, 1};
+        final int[] inputArray4 = new int[]{1, 4, 1, 4, 5};
+
         ArrayOfOnesAndFours oOF = new ArrayOfOnesAndFours();
-        oOF.inputArray =new int[] {1, 1, 1};
-        System.out.println(oOF.checkIsOnly1And4InArray(oOF.inputArray));
+        
+        System.out.println(oOF.checkIsOnly1And4InArray(inputArray1));
+        System.out.println(oOF.checkIsOnly1And4InArray(inputArray2));
+        System.out.println(oOF.checkIsOnly1And4InArray(inputArray3));
+        System.out.println(oOF.checkIsOnly1And4InArray(inputArray4));
     }
 
     public boolean checkIsOnly1And4InArray(int[] inputArray) {
@@ -19,7 +25,7 @@ public class ArrayOfOnesAndFours {
         boolean otherNumbers = false;
         boolean is1 = false;
         boolean is4 = false;
-        for (int i = 0; i <  inputArray.length; i++) {
+        for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i] != 1 && inputArray[i] != 4) {
                 otherNumbers = true;
                 break;
