b8ac9e56ea162a4576d70221785353f6ba9802f7
alekoChe <aleko2117bis@gmail.com>
2021-12-14 09:48:34 +0300

    добавил проверку выброса исключения

--- src/test/java/tecting/ArraysTransformatorTest.java
+++ src/test/java/tecting/ArraysTransformatorTest.java
@@ -3,6 +3,7 @@ package tecting;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +13,7 @@ public class ArraysTransformatorTest {
     private int[] arr2 = {1, 2, 3, 4, 5, 6,7, 8};
     private int[] arr3 = {4, 1, 4, 5, 6, 7 , 8};
     private int[] arr4 = {0, 1, 2, 4, 1, 4, 5, 6, 7 , 8};
+    private int[] arr5 = {1, 2, 3};
 
     @BeforeClass
     public static void init() {
@@ -31,12 +33,22 @@ public class ArraysTransformatorTest {
     }
     @Test
     public void transformatorArrayTest3() {
+        /** несколько четверок в массиве */
         final int[] result = transformator.transformArray(arr3);
         Assert.assertArrayEquals(new int[]{5, 6,7,8}, result);
     }
     @Test
     public void transformatorArrayTest4() {
+        /** несколько четверок в массиве */
         final int[] result = transformator.transformArray(arr4);
         Assert.assertArrayEquals(new int[]{5, 6,7,8}, result);
+    }
+    @Test
+    public void checkException() {
+        /** проверка на выброс исключения если нет четверки в массиве */
+        Assertions.assertThrows(Not4Exception.class, () -> {
+            transformator.transformArray(arr5);
+        });
+
     }
 }
