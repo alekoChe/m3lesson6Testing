d6d09c6eccd8be3a20c0d76aae6f18da03376163
alekoChe <aleko2117bis@gmail.com>
2021-12-14 09:23:02 +0300

    add class ArraysTransformatorTest

--- src/test/java/tecting/ArraysTransformatorTest.java
+++ src/test/java/tecting/ArraysTransformatorTest.java
@@ -1,6 +1,8 @@
 package tecting;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -15,5 +17,26 @@ public class ArraysTransformatorTest {
     public static void init() {
         System.out.println("Init");
         transformator = new ArraysTransformator();
+    }
+
+    @Test
+    public void transformatorArrayTest() {
+        final int[] result = transformator.transformArray(arr);
+        Assert.assertArrayEquals(new int[]{5, 6}, result);
+    }
+    @Test
+    public void transformatorArrayTest2() {
+        final int[] result = transformator.transformArray(arr2);
+        Assert.assertArrayEquals(new int[]{5, 6,7,8}, result);
+    }
+    @Test
+    public void transformatorArrayTest3() {
+        final int[] result = transformator.transformArray(arr3);
+        Assert.assertArrayEquals(new int[]{5, 6,7,8}, result);
+    }
+    @Test
+    public void transformatorArrayTest4() {
+        final int[] result = transformator.transformArray(arr4);
+        Assert.assertArrayEquals(new int[]{5, 6,7,8}, result);
     }
 }
